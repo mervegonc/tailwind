9a074da80228355f63bdc975a55e7003f26e71a5
merve-goncu <merve.gnc.946@gmail.com>
2024-08-23 16:09:07 +0300

    tailwind-finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./public/**/*.{html,js}"],
+  darkMode: 'class',
+  
   theme: {
-    extend: {},
+    container:{
+      center:true,
+      screens:{
+        lg:'1140px',
+        xl:'1140px',
+        '2xl':'1140px'
+      }
+    },
+    extend: {fontFamily:{
+      gemunu: ['Gemunu Libre', 'sans-serif'],
+      open: ['Open Sans', 'sans-serif']
+    },
+    colors: {
+      't-red': '#BC1A45',
+      'black': '#000000',
+      't-melon': '#FFD369',
+      't-grey': '#DDDDDD',
+      't-white': '#F7F7F7',
+      'bg-transparent': 'transparent',
+    },
+    spacing:{
+      128 :'32rem',
+    },
+  
+  
+  
+  
+  },
+   
+    
   },
   plugins: [],
 }
